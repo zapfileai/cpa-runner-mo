8556511b9f842eb9c505251bd2412e9e0387fba8
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:19:15 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
-﻿const missouriCities = [
-  // paste first 80 cities from cities.csv here
+const missouriCities = [
+  "Agency", "Albany", "Amoret", "Appleton City", "Archie",
+  "Argyle", "Arnold", "Ash Grove", "Ashland", "Augusta",
+  "Aurora", "Auxvasse", "Ava", "Ballwin", "Barnhart",
+  "Bates City", "Beaufort", "Belgrade", "Bell City", "Belle",
+  "Belton", "Benton City", "Berger", "Bernie", "Bethany",
+  "Billings", "Bloomsdale", "Blue Springs", "Bois D'Arc", "Bolivar",
+  "Bonne Terre", "Bonnots Mill", "Boonville", "Bourbon", "Bowling Green",
+  "Branson", "Bridgeton", "Brookfield", "Brookline", "Bucklin",
+  "Buckner", "Buffalo", "Burfordville", "Butler", "Cabool",
+  "California", "Callao", "Camdenton", "Cameron", "Canton",
+  "Cape Girardeau", "Carl Junction", "Carrollton", "Carterville", "Carthage",
+  "Caruthersville", "Cassville", "Cedar Hill", "Centertown", "Centerview",
+  "Centralia", "Chaffee", "Chamois", "Charleston", "Chesterfield",
+  "Chilhowee", "Chillicothe", "Clark", "Clarksville", "Clarkton",
+  "Clever", "Clinton", "Cole Camp", "Columbia", "Concordia",
+  "Crane", "Crystal City", "Cuba", "De Kalb", "De Soto",
 ];
 
 const BATCH_SIZE = 5;
